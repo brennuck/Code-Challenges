0ba13e9ca36ce8eab9387a4c9c9157cb334758c4
Brennon Nuckols <brennuck@gmail.com>
2020-09-10 12:40:37 -0600

    challenge

---
+++ Easy/arrayChange.js
@@ -0,0 +1,14 @@
+// You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+
+function arrayChange(inputArray) {
+    let count = 0;
+    
+    for (let i = 0; i < inputArray.length - 1; i++) {
+        if (inputArray[i] >= inputArray[i + 1]) {
+            let difference = inputArray[i] + 1 - inputArray[i + 1];
+            inputArray[i + 1] = inputArray[i] + 1
+            count += difference;
+        }
+    }
+    return count
+}
