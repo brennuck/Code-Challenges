5808a29d96aac5a6952ce6a3d249d2dcfcd1952b
Brennon Nuckols <brennuck@gmail.com>
2020-10-08 09:58:28 -0600

    divisible sum pairs

---
+++ Easy/DivisibleSumPairs.js
@@ -0,0 +1,21 @@
+// Complete the divisibleSumPairs function in the editor below. It should return the integer count of pairs meeting the criteria.
+
+// divisibleSumPairs has the following parameter(s):
+
+// n: the integer length of array ar
+// ar: an array of integers
+// k: the integer to divide the pair sum by
+
+function divisibleSumPairs(n, k, ar) {
+	let count = 0;
+
+	for (let i = 0; i < ar.length; i++) {
+		for (let j = i + 1; j < ar.length; j++) {
+			if ((ar[i] + ar[j]) % k === 0) {
+				count += 1;
+			}
+		}
+	}
+
+	return count;
+}
