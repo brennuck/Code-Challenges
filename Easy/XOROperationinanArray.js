6ee8212da4477d1856072da06400c1e582e4e02f
Brennon Nuckols <brennuck@gmail.com>
2020-12-03 11:18:18 -0700

    ad

---
+++ Easy/XOROperationinanArray.js
@@ -0,0 +1,15 @@
+// Given an integer n and an integer start.
+
+// Define an array nums where nums[i] = start + 2*i (0-indexed) and n == nums.length.
+
+// Return the bitwise XOR of all elements of nums.
+
+var xorOperation = function (n, start) {
+	let temp;
+
+	for (let i = 0; i < n; ++i) {
+		temp ^= start + 2 * i;
+	}
+
+	return temp;
+};
